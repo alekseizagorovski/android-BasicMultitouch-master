45975ee6d5686fe96471cb868f9b8c43a480497f
Aleksei <aleksei@polyston.ee>
2015-04-25 17:21:41 +0300

    TouchDisplay

--- Application/src/main/java/com/example/android/basicmultitouch/TouchDisplayView.java
+++ Application/src/main/java/com/example/android/basicmultitouch/TouchDisplayView.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.android.basicmultitouch;
 
@@ -28,11 +14,7 @@ import android.view.View;
 
 import com.example.android.basicmultitouch.Pools.SimplePool;
 
-/**
- * View that shows touch events and their history. This view demonstrates the
- * use of {@link #onTouchEvent(android.view.MotionEvent)} and {@link android.view.MotionEvent}s to keep
- * track of touch pointers across events.
- */
+
 public class TouchDisplayView extends View {
 
     // Hold data for active touch pointer IDs
